9c604c322358d6d773c426bb085b07af5a18f09d
Artem Pulkin <artem.pulkin@epfl.ch>
2017-04-05 12:45:58 +0200

    Fixed possible performance issues

--- dft/numint.py
+++ dft/numint.py
@@ -164,7 +164,8 @@ def eval_rho(mol, ao, dm, non0tab=None, xctype='LDA', verbose=None):
     >>> dm = dm + dm.T
     >>> rho, dx_rho, dy_rho, dz_rho = eval_rho(mol, ao, dm, xctype='LDA')
     '''
-    ao = numpy.ascontiguousarray(ao)
+    if not ao.flags.c_contiguous:
+        ao = numpy.ascontiguousarray(ao)
     xctype = xctype.upper()
     if xctype == 'LDA':
         ngrids, nao = ao.shape
@@ -238,7 +239,8 @@ def eval_rho2(mol, ao, mo_coeff, mo_occ, non0tab=None, xctype='LDA',
         if xctype = GGA;  (6,N) array for meta-GGA, where last two rows are
         \nabla^2 rho and tau = 1/2(\nabla f)^2
     '''
-    ao = numpy.ascontiguousarray(ao)
+    if not ao.flags.c_contiguous:
+        ao = numpy.ascontiguousarray(ao)
     xctype = xctype.upper()
     if xctype == 'LDA':
         ngrids, nao = ao.shape
@@ -361,7 +363,8 @@ def eval_mat(mol, ao, weight, rho, vxc,
         XC potential matrix in 2D array of shape (nao,nao) where nao is the
         number of AO functions.
     '''
-    ao = numpy.ascontiguousarray(ao)
+    if not ao.flags.c_contiguous:
+        ao = numpy.ascontiguousarray(ao)
     xctype = xctype.upper()
     if xctype == 'LDA':
         ngrids, nao = ao.shape
